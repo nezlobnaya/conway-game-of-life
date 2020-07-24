ca556a2c5eb01056302bec9f92edfdc334602407
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-24 19:13:03 -0400

    added Controls and Presets

---
+++ src/components/Presets.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Dropdown, Button } from 'semantic-ui-react';
+
+const options = [
+    { key: 'beacon', value: 'beacon', text: 'Beacon' },
+    { key: 'line', value: 'line', text: 'Line' },
+    { key: 'glider', value: 'glider', text: 'Glider' },
+    { key: 'pulsar', value: 'pulsar', text: 'Pulsar' },
+    { key: 'diehard', value: 'diehard', text: 'Diehard' },
+    { key: 'gliderGun', value: 'gliderGun', text: 'Gosper Glider Gun' }
+];
+
+class Presets extends React.Component {
+
+    state = {
+        preset: 'beacon'
+    }
+
+    onLoad = () => {
+        const { preset } = this.state;
+        return preset ? this.props.load(preset) : null;
+    }
+
+    render() {
+        return (
+            <div className='presets'>
+                <Button as='div' labelPosition='left'>
+                    <Dropdown
+                        placeholder='Select a preset'
+                        options={options}
+                        selection
+                        className='label'
+                        value={this.state.preset}
+                        onChange={(e, { value }) => this.setState({ preset: value })}
+                    />
+                    <Button
+                        content='Load'
+                        onClick={this.onLoad}
+                        disabled={this.props.playing}
+                    />
+                </Button>
+            </div>
+        );
+    }
+
+}
+
+export default Presets;
