4c0d9e2165b98fedc65c1f95fb8bcc0a9719238c
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-29 15:39:41 -0400

    minor styling refactoring

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,22 +5,22 @@ const Rules = () => (
     <div>
       <ul>
         <h3 style={{ color: "#d4ae6f" }}>Intro</h3>
-        <li style={{ color: "#d4ae6f", fontSize: "17px", lineHeight: "130%"}} >
+        <li style={{ color: "#d4ae6f", fontSize: "17px", lineHeight: "140%"}} >
         The game takes place on a two-dimensional finite or infinite grid 
         whose cells can take two distinct states: “alive” or “dead”.
         </li>
         <h3 style={{ color: "#d4ae6f" }}>Rules for death</h3>
-        <li style={{ color: "#d4ae6f" , fontSize: "17px" }} >
+        <li style={{ color: "#d4ae6f" , fontSize: "17px", lineHeight: "140%" }} >
           If the cell is alive and has 2 or 3 neighbors, then it remains alive.
           Else it dies
         </li>
         <h3 style={{ color: "#d4ae6f" }}>Rules for birth</h3>
-        <li style={{ color: "#d4ae6f" , fontSize: "17px" }} >
+        <li style={{ color: "#d4ae6f" , fontSize: "17px", lineHeight: "140%" }} >
           If the cell is dead and has exactly 3 neighbors, then it comes to
           life. Else, it remains dead.
         </li>
         <h3 style={{ color: "#d4ae6f" }}>The Controls</h3>
-        <li style={{ color: "#d4ae6f" , fontSize: "17px" }} >
+        <li style={{ color: "#d4ae6f" , fontSize: "17px", lineHeight: "140%" }} >
         Choose a figure from the drop-down menu or make one yourself by clicking on the cells with a mouse. 
         A new generation of cells (corresponding to one iteration of the rules)
         is initiated by the 'Next' button. 
