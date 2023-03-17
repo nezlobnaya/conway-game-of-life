edcabff38b58c102aa979e09d3b929719dc34c86
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-16 21:03:56 -0400

    src/components/Main.js: Refactor interval, changeState, onNext logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { createWorld, GENERATION_TIME, nextGeneration, shuffle } from './Game';
 
 import About from './About';
@@ -12,8 +12,12 @@ const Game = () => {
   const [world, setWorld] = useState(loadPreset(''));
   const [generation, setGeneration] = useState(0);
   const [playing, setPlaying] = useState(false);
-  const [intervalId, setIntervalId] = useState(null);
+  // The useRef hook is better suited for storing mutable values that do not trigger re-renders when changed.
+  const intervalId = useRef(null);
 
+
+  // to avoid stale state in the onNext function,
+  // use the functional form of setState to ensure that we always have the latest state.
   const changeState = (newWorld, newGeneration) => {
     setWorld(newWorld);
     setGeneration(newGeneration);
@@ -27,26 +31,41 @@ const Game = () => {
 
   const onPreset = preset => changeState(loadPreset(preset), 0);
 
-  const onNext = () => onChange(nextGeneration(world));
+  // use the functional form of setWorld, 
+  // which ensures that we're always working with the latest world state. 
+  const onNext = () => {
+    setWorld((prevWorld) => {
+      const newWorld = nextGeneration(prevWorld);
+      onChange(newWorld);
+      return newWorld;
+    });
+  };
 
   const onPlay = () => {
     setPlaying(true);
-    const interval = setInterval(() => onNext(), GENERATION_TIME);
-    setIntervalId(interval);
+    intervalId.current = setInterval(onNext, GENERATION_TIME);
   };
 
   const onStop = () => {
     setPlaying(false);
-    clearInterval(intervalId);
+    clearInterval(intervalId.current);
+    intervalId.current = null;
   };
 
+
   useEffect(() => {
+    // This is an empty effect function that does not perform any side effects when the component mounts.
+
     return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
+      // This function is the cleanup function that runs when the component unmounts.
+
+      if (intervalId.current) {
+        // If there is an intervalId stored in the useRef variable, clear the interval.
+        clearInterval(intervalId.current);
       }
     };
-  }, [intervalId]);
+  }, []); // The empty dependency array ensures the effect function only runs on mount and the cleanup function only runs on unmount.
+
 
   return (
     <div>
